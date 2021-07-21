35da3b8dba1de3397a4a6cd68ea3b828ff8808e0
Jefferson Boothe <46006304+shamuraii@users.noreply.github.com>
2021-07-21 18:57:33 -0400

    CTC Iteration 3 (#9)
    * First Commit
    
    - Preparing for Iteration 3
    - Code is not complete but compiles
    
    * More Signals

---
+++ CTC/ctc_debugger.h
@@ -0,0 +1,33 @@
+#ifndef CTC_DEBUGGER_H
+#define CTC_DEBUGGER_H
+
+#include <QDialog>
+
+#include "trainnetwork.h"
+
+namespace Ui {
+class CtcDebugger;
+}
+
+class CtcDebugger : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit CtcDebugger(QWidget *parent = nullptr);
+    ~CtcDebugger();
+
+public slots:
+    void ReloadPage();
+    void RefreshTrainData();
+    void RefreshStationData();
+    void SetThroughput();
+signals:
+    void UpdatedThroughput(int persons);
+
+private:
+    Ui::CtcDebugger *ui;
+    TrainNetwork *network_;
+};
+
+#endif // CTC_DEBUGGER_H
