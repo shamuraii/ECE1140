76bc18f3107e61c78bdbaab53614257b357a249c
Jefferson Boothe <46006304+shamuraii@users.noreply.github.com>
2021-08-05 17:36:50 -0400

    Relative File Paths

--- ctc/ctc_constants.h
+++ ctc/ctc_constants.h
@@ -6,15 +6,15 @@
 
 double const kMPHperKMH = 0.621371;
 
-QString const kRedlineBlockFile = QStringLiteral("C:\\Users\\Jeff\\Documents\\ECE 1140\\CTC Repo\\redline_blocks.csv");
-QString const kRedlineStationFile = QStringLiteral("C:\\Users\\Jeff\\Documents\\ECE 1140\\CTC Repo\\redline_stations.csv");
-QString const kRedlineSwitchesFile = QStringLiteral("C:\\Users\\Jeff\\Documents\\ECE 1140\\CTC Repo\\redline_switches.csv");
+QString const kRedlineBlockFile = QStringLiteral("redline_blocks.csv");
+QString const kRedlineStationFile = QStringLiteral("redline_stations.csv");
+QString const kRedlineSwitchesFile = QStringLiteral("redline_switches.csv");
 QString const kRedlineName = QStringLiteral("Red Line");
 bool const kRedBool = 0;
 
-QString const kGreenlineBlockFile = QStringLiteral("C:\\Users\\Jeff\\Documents\\ECE 1140\\CTC Repo\\greenline_blocks.csv");
-QString const kGreenlineStationFile = QStringLiteral("C:\\Users\\Jeff\\Documents\\ECE 1140\\CTC Repo\\greenline_stations.csv");
-QString const kGreenlineSwitchesFile = QStringLiteral("C:\\Users\\Jeff\\Documents\\ECE 1140\\CTC Repo\\greenline_switches.csv");
+QString const kGreenlineBlockFile = QStringLiteral("greenline_blocks.csv");
+QString const kGreenlineStationFile = QStringLiteral("greenline_stations.csv");
+QString const kGreenlineSwitchesFile = QStringLiteral("greenline_switches.csv");
 QString const kGreenlineName = QStringLiteral("Green Line");
 bool const kGreenBool = 1;
 
